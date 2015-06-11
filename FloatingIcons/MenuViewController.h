cabf0d91b74d8d6e76bc4405763b13356f6e6490
Alain K <alain.kahwaji@gmail.com>
2015-06-11 10:39:48 -0700

    Initialize repo, basic setup for Storyboard

---
+++ FloatingIcons/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  FloatingIcons
+//
+//  Created by Alain  on 2015-06-11.
+//  Copyright (c) 2015 Alain . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UICollectionViewController
+
+@end
